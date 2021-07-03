71d8dd04d44d2d50864ee33d0cf980f652a8f5f9
asad0306 <qaisansari46@gmail.com>
2021-07-03 11:50:57 +0100

    .js

--- add-content.js
+++ add-content.js
@@ -1,29 +1,49 @@
-let today = new Date();
-let hourNow = today.getHours();
-let greeting;
+var slideIndex = 1;
+        showSlides(slideIndex);
 
-if (hourNow > 18) (
-    greeting = 'Good evening!'
-)
-else if (hourNow > 12) (
-    greeting = 'Good afternoon!'
-)
-else if (hourNow > 0) (
-    greeting = 'Good morning!'
-)
-else (
-    greeting = 'Welcome!'
-)
+        // Next/previous controls
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
 
-document.write('<h1>' + greeting + '<h1>')
+        // Thumbnail image controls
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
 
-let isGreater = 4 > 1;
-console.log(isGreater);
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {
+                slideIndex = 1
+            }
+            if (n < 1) {
+                slideIndex = slides.length
+            }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
 
-const max = 57;
-    const actual = max - 13;
-    const percentage  = actual / 3;
+        var myIndex = 0;
+        carousel();
 
-    console.log(percentage);
-
-
+        function carousel() {
+            var i;
+            var x = document.getElementsByClassName("mySlides");
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+            myIndex++;
+            if (myIndex > x.length) {
+                myIndex = 1
+            }
+            x[myIndex - 1].style.display = "block";
+            setTimeout(carousel, 2000); // Change image every 2 seconds
+             }
