32539ec0aa1405e1abe1c61345429d39430c7587
asad0306 <qaisansari46@gmail.com>
2021-08-24 05:33:46 +0100

    html/css

--- add-content.js
+++ add-content.js
@@ -42,9 +42,4 @@ function showSlides() {
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 } 
 
-const string1 = "A string primitive";
-const string2 = 'Also a string primitive';
-const string3 = `Yet another string primitive`;
-const string4 = new String ("A String object");
-
-console.log(string4);
+console.log("Hello, Wolrd!");
